41441e8a6c2a9278b79c24e4256525645d18db28
clifford dicarlo <cdc4444@gmail.com>
2018-07-14 19:27:04 -0400

    Initial Commit

---
+++ continuingAgain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  continuingAgain
+//
+//  Created by Clifford Campo on 7/14/18.
+//  Copyright © 2018 CliffordCampo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
